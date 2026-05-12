40d7d8074f07306951360ed6e0f10f718f01165e
Dima <Dima@DESKTOP-058A3R7>
2026-05-12 13:28:32 +0300

    insert btn& statments

--- DB/Form1.Designer.cs
+++ DB/Form1.Designer.cs
@@ -37,6 +37,7 @@
             this.hospitalDataSet = new DB.HospitalDataSet();
             this.pATIENTTableAdapter = new DB.HospitalDataSetTableAdapters.PATIENTTableAdapter();
             this.showData = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pATIENTBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.hospitalDataSet)).BeginInit();
@@ -51,11 +52,12 @@
             this.cONTACTHISTORYDataGridViewTextBoxColumn,
             this.dEMOGRAPHICDataGridViewTextBoxColumn});
             this.dataGridView1.DataSource = this.pATIENTBindingSource;
-            this.dataGridView1.Location = new System.Drawing.Point(288, 83);
+            this.dataGridView1.Location = new System.Drawing.Point(37, 37);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(500, 210);
+            this.dataGridView1.Size = new System.Drawing.Size(429, 188);
             this.dataGridView1.TabIndex = 1;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
@@ -99,21 +101,34 @@
             // 
             // showData
             // 
-            this.showData.Location = new System.Drawing.Point(629, 346);
+            this.showData.Location = new System.Drawing.Point(167, 281);
+            this.showData.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.showData.Name = "showData";
-            this.showData.Size = new System.Drawing.Size(105, 23);
+            this.showData.Size = new System.Drawing.Size(79, 19);
             this.showData.TabIndex = 2;
             this.showData.Text = "show data";
             this.showData.UseVisualStyleBackColor = true;
             this.showData.Click += new System.EventHandler(this.showData_Click);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(60, 279);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Insert";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.insert_Click);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.showData);
             this.Controls.Add(this.dataGridView1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load_1);
@@ -133,6 +148,7 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn cONTACTHISTORYDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn dEMOGRAPHICDataGridViewTextBoxColumn;
         private System.Windows.Forms.Button showData;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- DB/Form1.cs
+++ DB/Form1.cs
@@ -1,27 +1,28 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
 using System.Data.SqlClient;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace DB
 {
     public partial class Form1 : Form
     {
+        string connectionString =
+            @"Data Source=DESKTOP-058A3R7\MSSQLSERVER01;Initial Catalog=AA;Integrated Security=True;Encrypt=True;TrustServerCertificate=True";
+
         public Form1()
         {
             InitializeComponent();
+
+           
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            SqlConnection sqlcon = new SqlConnection("Data Source=LAPTOP-N057VS3J\\SQLEXPRESS;Initial Catalog=Hospital;Integrated Security=True");
+          
         }
+
         private void label1_Click(object sender, EventArgs e)
         {
 
@@ -29,18 +30,69 @@ namespace DB
 
         private void Form1_Load_1(object sender, EventArgs e)
         {
-            // TODO: This line of code loads data into the 'hospitalDataSet.PATIENT' table. You can move, or remove it, as needed.
 
+        }
 
+        private void LoadPatients()
+        {
+            string query = "SELECT * FROM PATIENT";
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+
+                SqlDataAdapter adapter = new SqlDataAdapter(query, con);
+                DataTable table = new DataTable();
+                adapter.Fill(table);
+
+                dataGridView1.DataSource = table;
+            }
         }
 
         private void showData_Click(object sender, EventArgs e)
         {
-            this.pATIENTTableAdapter.Fill(this.hospitalDataSet.PATIENT);
+         
+
+            LoadPatients();
         }
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
+
+        }
+
+        private void ExecuteQuery(string query)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+
+                SqlCommand cmd = new SqlCommand(query, con);
+              ;
+                int rows = cmd.ExecuteNonQuery();
+            }
+
+          
+        }
+
+        private void insert_Click(object sender, EventArgs e)
+        {
+            string insertPatients = @"
+        IF NOT EXISTS (SELECT 1 FROM PATIENT WHERE PATIENT_ID = 103)
+        INSERT INTO PATIENT (PATIENT_ID, CONTACT_HISTORY, DEMOGRAPHIC)
+        VALUES (103, '01018532698', 'Male, 99 years old');
+
+        IF NOT EXISTS (SELECT 1 FROM PATIENT WHERE PATIENT_ID = 104)
+        INSERT INTO PATIENT (PATIENT_ID, CONTACT_HISTORY, DEMOGRAPHIC)
+        VALUES (104, '01016813826', 'Male, 48 years old');
+
+        IF NOT EXISTS (SELECT 1 FROM PATIENT WHERE PATIENT_ID = 105)
+        INSERT INTO PATIENT (PATIENT_ID, CONTACT_HISTORY, DEMOGRAPHIC)
+        VALUES (105, '01005887614', 'Male, 36 years old');
+    ";
+
+            ExecuteQuery(insertPatients);
+            LoadPatients();
 
         }
     }
