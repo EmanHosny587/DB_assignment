b720710fdc609eff2890960febf02fa6b7a4a18f
EmanHosny587 <emanhosny492@gmail.com>
2026-05-12 11:55:44 +0300

    Add project files.

---
+++ DB/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace DB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.pATIENTIDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cONTACTHISTORYDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dEMOGRAPHICDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.pATIENTBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.hospitalDataSet = new DB.HospitalDataSet();
+            this.pATIENTTableAdapter = new DB.HospitalDataSetTableAdapters.PATIENTTableAdapter();
+            this.showData = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pATIENTBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hospitalDataSet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.pATIENTIDDataGridViewTextBoxColumn,
+            this.cONTACTHISTORYDataGridViewTextBoxColumn,
+            this.dEMOGRAPHICDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.pATIENTBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(288, 83);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(500, 210);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // pATIENTIDDataGridViewTextBoxColumn
+            // 
+            this.pATIENTIDDataGridViewTextBoxColumn.DataPropertyName = "PATIENT_ID";
+            this.pATIENTIDDataGridViewTextBoxColumn.HeaderText = "PATIENT_ID";
+            this.pATIENTIDDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.pATIENTIDDataGridViewTextBoxColumn.Name = "pATIENTIDDataGridViewTextBoxColumn";
+            this.pATIENTIDDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // cONTACTHISTORYDataGridViewTextBoxColumn
+            // 
+            this.cONTACTHISTORYDataGridViewTextBoxColumn.DataPropertyName = "CONTACT_HISTORY";
+            this.cONTACTHISTORYDataGridViewTextBoxColumn.HeaderText = "CONTACT_HISTORY";
+            this.cONTACTHISTORYDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.cONTACTHISTORYDataGridViewTextBoxColumn.Name = "cONTACTHISTORYDataGridViewTextBoxColumn";
+            this.cONTACTHISTORYDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // dEMOGRAPHICDataGridViewTextBoxColumn
+            // 
+            this.dEMOGRAPHICDataGridViewTextBoxColumn.DataPropertyName = "DEMOGRAPHIC";
+            this.dEMOGRAPHICDataGridViewTextBoxColumn.HeaderText = "DEMOGRAPHIC";
+            this.dEMOGRAPHICDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.dEMOGRAPHICDataGridViewTextBoxColumn.Name = "dEMOGRAPHICDataGridViewTextBoxColumn";
+            this.dEMOGRAPHICDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // pATIENTBindingSource
+            // 
+            this.pATIENTBindingSource.DataMember = "PATIENT";
+            this.pATIENTBindingSource.DataSource = this.hospitalDataSet;
+            // 
+            // hospitalDataSet
+            // 
+            this.hospitalDataSet.DataSetName = "HospitalDataSet";
+            this.hospitalDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // pATIENTTableAdapter
+            // 
+            this.pATIENTTableAdapter.ClearBeforeFill = true;
+            // 
+            // showData
+            // 
+            this.showData.Location = new System.Drawing.Point(629, 346);
+            this.showData.Name = "showData";
+            this.showData.Size = new System.Drawing.Size(105, 23);
+            this.showData.TabIndex = 2;
+            this.showData.Text = "show data";
+            this.showData.UseVisualStyleBackColor = true;
+            this.showData.Click += new System.EventHandler(this.showData_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.showData);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pATIENTBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hospitalDataSet)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private HospitalDataSet hospitalDataSet;
+        private System.Windows.Forms.BindingSource pATIENTBindingSource;
+        private HospitalDataSetTableAdapters.PATIENTTableAdapter pATIENTTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn pATIENTIDDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cONTACTHISTORYDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dEMOGRAPHICDataGridViewTextBoxColumn;
+        private System.Windows.Forms.Button showData;
+    }
+}
+
---
+++ DB/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DB
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SqlConnection sqlcon = new SqlConnection("Data Source=LAPTOP-N057VS3J\\SQLEXPRESS;Initial Catalog=Hospital;Integrated Security=True");
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'hospitalDataSet.PATIENT' table. You can move, or remove it, as needed.
+
+
+        }
+
+        private void showData_Click(object sender, EventArgs e)
+        {
+            this.pATIENTTableAdapter.Fill(this.hospitalDataSet.PATIENT);
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
